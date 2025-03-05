d23bf919a47d37c8f8262632bcb0dc350d2f6088
light <2394023336@qq.com>
2025-03-05 14:01:37 +0800

    新增商品价格卖盘数据
    新增商品价格卖盘数据

--- data/comm_real_news_data.py
+++ data/comm_real_news_data.py
@@ -19,6 +19,16 @@ def get_li_price_data(name,url='https://www.100ppi.com/vane/detail-733.html'):
     soup = BeautifulSoup(html_doc, 'html.parser')
     search_div = soup.find_all("div",'list-event')
 
+    divs = soup.find_all("div","colunm pr-bor")
+    sell_url = None
+    if divs and len(divs)>0:
+        pr_div = divs[2]
+        spans = pr_div.find_all("span")
+        if spans and len(spans)>0:
+            a_s = spans[0].find_all('a')
+            if a_s and len(a_s)>0:
+                sell_url = a_s[0]['href']
+
     headers = search_div[0]
     lis = headers.find_all('li')
     comm_head = 'http://www.100ppi.com'
@@ -38,7 +48,7 @@ def get_li_price_data(name,url='https://www.100ppi.com/vane/detail-733.html'):
         new_url = f"{comm_head}{a.attrs['href']}"
         dict_data = {"time":time_text,"header":header_text,"data_type":data_type,"name":name,"url":new_url}
         datas.append(dict_data)
-    return datas
+    return datas,sell_url
 
 
 def get_100ppi_detail_new_data(url):
@@ -73,6 +83,7 @@ def get_all_detail_data(names=None):
     header_url = "http://www.100ppi.com"
     tables = headers.find_all('table')
     ret_news = {}
+    sell_urls = {}
     for table in tables:
         trs = table.find_all('tr')
         for tr in trs[1:]:
@@ -84,12 +95,13 @@ def get_all_detail_data(names=None):
                 name = urls[0].text.replace(' ', '').replace("\n", '')
                 if names is not None and name in names:
                     try:
-                        x = try_get_action(get_li_price_data,try_count=3,name=name,url=url)
+                        x,sell_url = try_get_action(get_li_price_data,try_count=3,name=name,url=url)
                         ret_news[name] = x
+                        sell_urls[name] = sell_url
                     except Exception as e:
                         print(e)
                         print(url)
-    return ret_news
+    return ret_news,sell_urls
 def stock_telegraph_cls_news():
     stock_telegraph_cls_df = try_get_action(ak.stock_info_global_cls,try_count=3)
     datas = []
@@ -109,4 +121,5 @@ def stock_telegraph_cls_news():
 
 
 if __name__ == '__main__':
-    pass
+    news,url = get_all_detail_data(names=['碳酸锂','轻质纯碱','玻璃'])
+    print(news,url)
---
+++ data/goods_sell_buy_order_data.py
@@ -0,0 +1,96 @@
+import sys
+import os
+
+# 可以在该目录之前执行该程序，否则会报引用工程包不存在
+sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+import requests
+from bs4 import BeautifulSoup
+from data.mongodb import get_mongo_table
+from pymongo import UpdateOne
+from datetime import datetime
+from utils.tool import mongo_bulk_write_data
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 设置中文显示不乱码
+plt.rcParams['font.sans-serif'] = ['Arial Unicode MS']
+import warnings
+
+warnings.filterwarnings('ignore')
+import re
+import schedule
+import time
+from data.comm_real_news_data import get_all_detail_data
+
+
+def handle_goods_price_data():
+    names = ['玻璃','沥青','轻质纯碱']
+    _,sell_url_dict = get_all_detail_data(names=names)
+    sell_buy_order = get_mongo_table(database='futures', collection='sell_buy_order')
+    for name,url in sell_url_dict.items():
+        print(f'handle url {url}')
+        respond = requests.get(url, headers={
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+            "accept-language": "an,zh-CN;q=0.9,zh;q=0.8,en;q=0.7"})
+        html = respond.content
+        html_doc = str(html, 'utf-8')  # html_doc=html.decode("utf-8","ignore")
+        soup = BeautifulSoup(html_doc, 'html.parser')
+        main_div = soup.find('div', id='main')
+
+        year = datetime.now().strftime("%Y")
+        before_year = str(int(year) - 1)
+        month = datetime.now().strftime("%m")
+
+        if main_div is not None:
+            tables = main_div.find_all("table")
+            if tables is not None and len(tables) > 0:
+                table = tables[0]
+                trs = table.find_all("tr")
+                mapping = {
+                    "卖盘商品": "sell_name",
+                    "规格": "specifications",
+                    "单价": "price",
+                    "数量": "quantity",
+                    "现货类型": "spot_type",
+                    "交收期": "delivery_period",
+                    "交货地": "delivery_location",
+                    "发布时间": "release_time",
+                    "有效时间": "validity_period",
+                    "联系": "contact"
+                }
+                upsert_datas = []
+                for tr in trs[1:]:
+                    tds = tr.find_all("td")
+                    datas = []
+                    day = None
+                    for i, td in enumerate(tds):
+                        if i == 7:
+                            text_value = td.text.strip().replace("\n", "").replace("\t", "")
+                            dm = int(text_value.split("-")[0])
+                            if dm > int(month):
+                                day = f"{before_year}-{text_value}"
+                            else:
+                                day = f"{year}-{text_value}"
+                        else:
+                            text_value = td.text.replace(" ", "").strip().replace("\n", "").replace("\t", "")
+                        datas.append(text_value)
+                    if len(datas) > 0:
+                        dict_data = {}
+                        for k, v in zip(list(mapping.values()), datas):
+                            dict_data[k] = v
+                        dict_data['time'] = day
+                        upsert_datas.append(UpdateOne(
+                            {"sell_name": dict_data['sell_name'], "time": dict_data['time'],
+                             "price": dict_data['price'],"delivery_location":dict_data['delivery_location'],'specifications':dict_data['specifications']},
+                            {"$set": dict_data},
+                            upsert=True))
+                mongo_bulk_write_data(sell_buy_order, upsert_datas)
+
+
+
+if __name__ == '__main__':
+    handle_goods_price_data()
+    schedule.every(30).minutes.do(handle_goods_price_data)
+    while True:
+        schedule.run_pending()
+        time.sleep(10)
